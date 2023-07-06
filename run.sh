223fd6ccf56287782b75a98f0712470904adbd7a
saipranayteja <pranayteja.ch@algonox.com>
2023-07-06 20:02:26 +0530

    zsxdcvbsdf

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ for var in $(echo $1 | sed -e 's/,/ /g') ; do
   var_name=$(echo $var | awk -F : '{print $1}')
   parameter=$(echo $var | awk -F : '{print $2}')
   value=$(aws ssm get-parameter --name $parameter --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g')
+  echo $(var_name)=$(value)
 done
-
 
